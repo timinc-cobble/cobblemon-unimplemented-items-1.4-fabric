8bfe1fe1b45bc7540ff49c0b4175429a6cb0ca0b
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-08-01 23:55:28 -0700

    added dry root

---
+++ src/main/kotlin/us/timinc/mc/cobblemon/unimplementeditems/items/DryRoot.kt
@@ -0,0 +1,37 @@
+package us.timinc.mc.cobblemon.unimplementeditems.items
+
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
+import com.cobblemon.mod.common.item.CobblemonItemGroups
+import com.cobblemon.mod.common.pokemon.Pokemon
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.network.chat.Component
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.ItemStack
+import us.timinc.mc.cobblemon.unimplementeditems.ErrorMessages
+
+class DryRoot : PokemonItem(
+    FabricItemSettings()
+    .group(CobblemonItemGroups.MEDICINE_ITEM_GROUP)) {
+    override fun processInteraction(
+        itemStack: ItemStack,
+        player: Player,
+        target: PokemonEntity,
+        pokemon: Pokemon
+    ): InteractionResult {
+        var changedOne = false;
+        pokemon.evs.forEach{entry ->
+            if (entry.value > .0) {
+                changedOne = true;
+                pokemon.evs[entry.key] = 0
+            }
+        }
+
+        if (!changedOne) {
+            player.sendSystemMessage(Component.translatable(ErrorMessages.alreadyHasZeroEvs))
+            return InteractionResult.FAIL
+        }
+
+        return InteractionResult.SUCCESS
+    }
+}
