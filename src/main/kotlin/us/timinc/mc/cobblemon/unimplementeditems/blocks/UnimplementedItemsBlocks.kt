dab4415e51dfd57772388f5d4c8d7803fa373259
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-11-15 21:32:05 -0800

    added repel to functional blocks

--- src/main/kotlin/us/timinc/mc/cobblemon/unimplementeditems/blocks/UnimplementedItemsBlocks.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/unimplementeditems/blocks/UnimplementedItemsBlocks.kt
@@ -3,10 +3,12 @@
 package us.timinc.mc.cobblemon.unimplementeditems.blocks
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
 import net.minecraft.block.Blocks
 import net.minecraft.item.BlockItem
+import net.minecraft.item.ItemGroups
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import us.timinc.mc.cobblemon.unimplementeditems.UnimplementedItems
@@ -19,5 +21,9 @@ object UnimplementedItemsBlocks {
         Registry.register(
             Registries.ITEM, UnimplementedItems.myResourceLocation("repel"), BlockItem(REPEL, FabricItemSettings())
         )
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register { content ->
+            content.add(REPEL)
+        }
     }
 }
